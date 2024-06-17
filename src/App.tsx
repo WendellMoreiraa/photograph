36f7ceca48a90f55feb792365c951645c7b1d8c8
WendellMoreiraa <wendell.ufpel@gmail.com>
2024-06-17 19:54:00 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./lib/theme/index.ts";
+import Root from "./routes/Root.tsx";
+import Home from "./pages/Home/index.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [{ path: "/", element: <Home /> }],
+  },
+]);
 function App() {
   return (
-    <>
-      <div>Hello</div>
-    </>
+    <ChakraProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   );
 }
 
